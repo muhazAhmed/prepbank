cb80fe9d447c11315bb9f1f210e22b532682dfe9
muhazAhmed <muhazvla313@gmail.com>
2024-10-11 17:42:12 +0530

    wrapped with cookies

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./lib/ThemeContext.tsx";
+import { CookiesProvider } from "react-cookie";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <CookiesProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </CookiesProvider>
   </StrictMode>
 );
