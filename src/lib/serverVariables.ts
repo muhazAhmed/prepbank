34848f14918e7fa806c4e7a2355597c03373fbea
muhazAhmed <muhazvla313@gmail.com>
2024-10-19 18:50:20 +0530

    created new server variables

---
+++ src/lib/serverVariables.ts
@@ -0,0 +1,8 @@
+export const ServerVariables = {
+    user: {
+        loginUser: "/user/login",
+        registerUser: "/user/register",
+        updateUser: "/user/update/",
+        deleteUser: "/user/delete/"
+    }
+}
