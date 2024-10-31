8d551c2eec507be80d67bb7ac26e6d921cc9d641
muhazAhmed <muhazvla313@gmail.com>
2024-10-31 10:44:26 +0530

    added new data

---
+++ src/lib/data.tsx
@@ -0,0 +1,42 @@
+import { MdInfoOutline, MdOutlinePhone } from "react-icons/md";
+import { MenuItem, MoreMenuOption } from "./prop";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { TfiMore } from "react-icons/tfi";
+import { GoGear } from "react-icons/go";
+
+export const navMenuItem: MenuItem[] = [
+    { id: 0, label: "Home", icon: "fa-solid fa-house", url: "/" },
+    { id: 1, label: "About", icon: "fa-solid fa-info-circle", url: "/about" },
+    { id: 2, label: "Contact", icon: "fa-solid fa-phone", url: "/contact" },
+    { id: 3, label: "Settings", icon: "fa-solid fa-gear", url: "/settings" },
+];
+
+export const sidebarData: MenuItem[] = [
+    { id: 0, label: "Dashboard", icon: <LuLayoutDashboard />, url: "/dashboard" },
+    { id: 1, label: "something", icon: <LuLayoutDashboard />, url: "/dashboard" },
+    { id: 2, label: "something else", icon: <LuLayoutDashboard />, url: "/dashboard" },
+    { id: 3, label: "another thing", icon: <LuLayoutDashboard />, url: "/dashboard" },
+    { id: 4, label: "yet another thing", icon: <LuLayoutDashboard />, url: "/dashboard" },
+    { id: 5, label: "last thing", icon: <LuLayoutDashboard />, url: "/dashboard" },
+]
+
+
+type BottomMenuOption = MenuItem | MoreMenuOption;
+export const bottomMenuOptions: BottomMenuOption[] = [
+    { id: 0, url: "/dashboard", icon: <LuLayoutDashboard className="text-xl" />, label: "Dashboard", type: "main" },
+    { id: 1, url: "/about", icon: <MdInfoOutline className="text-xl" />, label: "About", type: "main" },
+    { id: 2, url: "/contact", icon: <MdOutlinePhone className="text-xl" />, label: "Contact", type: "main" },
+    { id: 3, url: "/settings", icon: <GoGear className="text-xl" />, label: "Settings", type: "main", authRequired: true },
+    { id: 4, url: "", icon: <TfiMore className="text-2xl" />, label: "More", type: "toggle" },
+    {
+        id: 5,
+        type: "more",
+        options: [
+            { id: 6, url: "/option1", icon: <LuLayoutDashboard className="text-xl" />, label: "Option 1" },
+            { id: 7, url: "/option2", icon: <LuLayoutDashboard className="text-xl" />, label: "Option 2" },
+            { id: 8, url: "/option3", icon: <LuLayoutDashboard className="text-xl" />, label: "Option 3" },
+            { id: 9, url: "/option4", icon: <LuLayoutDashboard className="text-xl" />, label: "Option 4" },
+            { id: 10, url: "/option5", icon: <LuLayoutDashboard className="text-xl" />, label: "Option 5" }
+        ]
+    }
+];
